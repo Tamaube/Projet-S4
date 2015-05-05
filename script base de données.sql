059e60578a0b334dbc17dba4643db62a24bd1b8d
Corentin Berthelet <corentin.berthelet@u-psud.fr>
2015-05-05 09:37:55 +0200

    script sql
    rajout clé étrangère

--- script base de données.sql
+++ script base de données.sql
@@ -21,20 +21,39 @@ CREATE TABLE produit
 	prix FLOAT(10),
 	coup_coeur BOOLEAN(1),
 	stock INT(30),
-	description VARCHAR(255)
+	description VARCHAR(255),
+	avis INT NOT NULL,
+	CONSTRAINT numAvis
+		FOREIGN KEY (avis)
+		REFERENCES avis(id),
+	catégorie INT NOT NULL,
+	CONSTRAINT numCatégorie
+		FOREIGN KEY (catégorie)
+		REFERENCES catégorie(id),
 );
 
 CREATE TABLE commande(
 	id INT(10) PRIMARY KEY NOT NULL,
+	client INT NOT NULL, 
+	produit INT NOT NULL,
 	quantité INT(50),
-	prix FLOAT(50),
 	date DATE,
-	statut VARCHAR(30)
+	statut VARCHAR(30),
+	CONSTRAINT userID
+		FOREIGN KEY (client)
+		REFERENCES utilisateur(id),
+	CONSTRAINT numProduit
+		FOREIGN KEY (produit)
+		REFERENCES produit(id),
 );
 
 CREATE TABLE avis(
 	id INT(10) PRIMARY KEY NOT NULL,
-	contenu VARCHAR(255)
+	client INT NOT NULL,
+	contenu VARCHAR(255),
+	CONSTRAINT userID
+		FOREIGN KEY (client)
+		REFERENCES utilisateur(id),
 );
 
 CREATE TABLE catégorie(
@@ -47,4 +66,14 @@ CREATE TABLE sous_categorie(
 	nom VARCHAR(30)
 );
 
+CREATE TABLE ProduitSousCat(
+	souscatégorie INT NOT NULL,
+	CONSTRAINT numCatégorie
+		FOREIGN KEY (souscatégorie)
+		REFERENCES sous_categorie(id),
+	produit INT NOT NULL,
+	CONSTRAINT numProduit
+		FOREIGN KEY (produit)
+		REFERENCES produit(id)
+);
 
