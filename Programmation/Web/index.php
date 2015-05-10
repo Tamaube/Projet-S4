eb39dec19cb9c0e01189f82b31a3bfb49b96f6cc
Elisa Lescarret <lescarretelisa@yahoo.fr>
2015-05-10 22:45:49 +0200

    bootstrap

--- Programmation/Web/index.php
+++ Programmation/Web/index.php
@@ -3,8 +3,33 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>ROCK'N'CLOTHES</title>
+		<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<script src="lib/jquery-1.11.2.min.js"></script>
+		<script src="lib/jquery-migrate-1.2.1.min.js"></script>
+		<script src="lib/bootstrap/js/jquery.js"></script>
+		<script src="lib/bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<?php require_once('view/header.php'); ?>
+		<div id="content">
+			 <nav class="navbar navbar-default">
+				<ul class="nav navbar-nav">
+					<li> <a href="#">Accueil</a> </li>
+					<li> <a href="#">Liens</a> </li>
+					<li class="dropdown"> 
+						<a data-toggle="dropdown" href="view/header.php">Témoignages<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Dompteurs</a></li>
+							<li><a href="#">Zoos</a></li>
+							<li><a href="#">Chasseurs</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Autres témoignages</a></li>
+						</ul>
+					</li>
+					<li> <a href="#">Références</a> </li>
+				</ul>
+			</nav>
+		</div>
+		
+
 	</body>
 </html>
