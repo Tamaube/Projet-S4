61f967da0db28a9808e1d3a727f7b7b796dd766d
CyrilKeil <cyril.keil@u-psud.fr>
2015-06-12 15:53:06 +0200

    Code cyril

---
+++ Programmation/Appli/partie Cyril/appliProjet/src/controller/ControleurAdministration.java
@@ -0,0 +1,8 @@
+package controller;
+
+import modele.GestionCategorie;
+
+public class ControleurAdministration{
+	public ControleurGeneral sonControleurGeneral;
+	public GestionCategorie saGestionDeCategorie;
+}
---
+++ Programmation/Appli/partie Cyril/appliProjet/src/modele/GestionCategorie.java
@@ -0,0 +1,20 @@
+package modele;
+
+import controller.ControleurAdministration;
+
+public class GestionCategorie {
+	public ControleurAdministration sonControleurAdministration;
+	/** Methodes **/
+	public void supprimerCategorie(int numCat) {
+		
+	}
+	
+	public void ajouterCategorie(String nomCat){
+		
+	}
+	
+	public void modifierCategorie(int numCat, String nomCat){
+		
+	}
+	
+}
