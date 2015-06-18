5189b8487fc1c432e445b60aa021d79d584636f7
Corentin Berthelet <corentin.berthelet@u-psud.fr>
2015-06-18 08:27:56 +0200

    test réparation pb

--- Programmation/Web/js/main.js
+++ Programmation/Web/js/main.js
@@ -73,16 +73,73 @@ $(document).ready(function(){
 		 return false;
 	});
 	
-	$(document).on('click','[data-toggle="ajaxModal"]',
-              function(e) {
-                $('#ajaxModal').remove();
-                e.preventDefault();
-                var $this = $(this)
-                  , $remote = $this.data('remote') || $this.attr('href')
-                  , $modal = $('<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>');
-                $('body').append($modal);
-                $modal.modal({backdrop: 'static', keyboard: false});
-                $modal.load($remote);
-    });
+	
+	$(document).on('click', '.suppPanier', function(){
+		
+		var reload = 'controller/controllerSeeAllCommande.php';
+
+		jQuery.ajax
+		({
+			url: $(this).attr('data-url'), // URL de la page de traitement
+			type: $(this).attr('data-method'), // Method du formulaire (POST GET etc ...)
+			data: {idCmd: $(this).attr('data-id')},
+			success: function(data)
+			{
+				if(data != "")
+				{
+					alert(data);
+				} else {
+					$('#corps_page').load(reload);
+				}
+			},
+			error: function(data, textStatus)
+			{
+				alert(textStatus);
+			}
+		});
+		 return false;
+	});
+	
+	$(document).on('click', '#verifPanier', function(){		
+		jQuery.ajax
+		({
+			url: $(this).attr('data-url'), // URL de la page de traitement
+			type: 'post', // Method du formulaire (POST GET etc ...)
+			data: $('#formPanier').serializeArray(),
+			dataType: 'json',
+			success: function(data)
+			{
+				if(data.message)
+				{
+					alert(data.message);
+				} else {
+					$('.fieldPastCmd').show();
+					$('#rue').html(data.rue);
+					$('#ville').val(data.ville);
+					$('#code_postal').val(data.code_postal);
+					$('#pays').val(data.pays);
+					$('#fieldVerifPAnier').hide();
+				}
+				
+			},
+			error: function(data, textStatus)
+			{
+				alert(textStatus);
+			}
+		});
+		 return false;
+	});
+	
+	// $(document).on('click','[data-toggle="ajaxModal"]',
+              // function(e) {
+                // $('#ajaxModal').remove();
+                // e.preventDefault();
+                // var $this = $(this)
+                  // , $remote = $this.data('remote') || $this.attr('href')
+                  // , $modal = $('<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>');
+                // $('body').append($modal);
+                // $modal.modal({backdrop: 'static', keyboard: false});
+                // $modal.load($remote);
+    // });
 	
 });	
